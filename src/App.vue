70b0178de88bbc0ad3093d5c3da7284565edc30b
Lordovik <38624924+Lordovik@users.noreply.github.com>
2019-10-22 19:56:33 +0300

    created template

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+    <div id="app">
+        <p id="timer">10s</p>
+        <div id="main">
+          <div id="green"></div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            color: "green"
+        };
+    }
+}
+</script>
+
+<style>
+#main{
+    background-color: grey;
+    height: 400px;
+    width: 400px;
+    border-radius: 50%;
+}
+</style>
---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const path = require('path');
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  resolve: {
+    extensions: ['.js', '.vue', '.json'],
+    alias: {
+    'vue$': 'vue/dist/vue.esm.js',
+    }
+    },
+  
+  watch: true,
+  
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    port: 3000,
+    open: true
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'vue-style-loader',
+          'css-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new VueLoaderPlugin(),
+    new HtmlWebpackPlugin({
+      template: "./index.html"
+    })
+  ]
+};
